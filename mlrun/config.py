6350c4e3ab87c5ac442d195c45b5f9415da59eb4
alonmr <alonnmaor@gmail.com>
2025-05-06 11:54:13 +0300

    test

--- mlrun/config.py
+++ mlrun/config.py
@@ -924,32 +924,33 @@ class Config:
 
     def update(self, cfg, skip_errors=False):
         for key, value in cfg.items():
-            if hasattr(self, key):
-                if isinstance(value, dict):
-                    # ignore the `skip_errors` flag here
-                    # if the key does not align with what mlrun config expects it is a user
-                    # input error that can lead to unexpected behavior.
-                    # raise the exception to ensure configuration is loaded correctly and do not
-                    # ignore any errors.
-                    config_value = getattr(self, key)
-                    # try:
-                    #     config_value.update(value)
-                    # except AttributeError as exc:
-                    #     if not isinstance(config_value, (dict, Config)):
-                    #         raise ValueError(
-                    #             f"Can not update `{key}` config. "
-                    #             f"Expected a configuration but received {type(value)}"
-                    #         ) from exc
-                    #     raise exc
-                else:
-                    try:
-                        setattr(self, key, value)
-                    except mlrun.errors.MLRunRuntimeError as exc:
-                        if not skip_errors:
-                            raise exc
-                        print(
-                            f"Warning, failed to set config key {key}={value}, {mlrun.errors.err_to_str(exc)}"
-                        )
+            pass
+            # if hasattr(self, key):
+            #     if isinstance(value, dict):
+            #         # ignore the `skip_errors` flag here
+            #         # if the key does not align with what mlrun config expects it is a user
+            #         # input error that can lead to unexpected behavior.
+            #         # raise the exception to ensure configuration is loaded correctly and do not
+            #         # ignore any errors.
+            #         config_value = getattr(self, key)
+            #         try:
+            #             config_value.update(value)
+            #         except AttributeError as exc:
+            #             if not isinstance(config_value, (dict, Config)):
+            #                 raise ValueError(
+            #                     f"Can not update `{key}` config. "
+            #                     f"Expected a configuration but received {type(value)}"
+            #                 ) from exc
+            #             raise exc
+            #     else:
+            #         try:
+            #             setattr(self, key, value)
+            #         except mlrun.errors.MLRunRuntimeError as exc:
+            #             if not skip_errors:
+            #                 raise exc
+            #             print(
+            #                 f"Warning, failed to set config key {key}={value}, {mlrun.errors.err_to_str(exc)}"
+            #             )
 
     def dump_yaml(self, stream=None):
         return yaml.dump(self._cfg, stream, default_flow_style=False)
